93414e04c696f057c216dd6fd716c0a3c04b89de
kuzhang <kkdezxx@gmail.com>
2023-08-30 13:38:59 +0400

    add hpc config files

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=transbathy
+#SBATCH --time=02:00:00
+#SBATCH --partition=gpu
+#SBATCH --account=kuin0084
+#SBATCH --output=transbathy.%j.out
+#SBATCH --error=transbathy.%j.err
+
+module load cuda/11.2
+module load miniconda/3
+conda activate transbath
+cd ~/code/Transbathy
+python main.py
