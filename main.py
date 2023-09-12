b32c75e1a5a63e09681effbe55eeeda94ab643db
kuzhang <kkdezxx@gmail.com>
2023-09-12 14:58:50 +0400

    hpc training

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from model import TransBath
 
 def run():
     # Loading configurations in yaml file
-    with open('config/config-gpu.yaml', 'r') as file:
+    with open('config/config-hpu.yaml', 'r') as file:
         config = yaml.safe_load(file)
     phase = config['Phase']
 
