1774db8913b3011e8b30630886984ccfd812e40a
kuzhang <kkdezxx@gmail.com>
2023-09-26 17:02:04 +0400

    update cpu training

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
 #SBATCH --job-name=transbathy
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --partition=gpu
 #SBATCH --account=kuin0084
 #SBATCH --output=transbathy.%j.out
